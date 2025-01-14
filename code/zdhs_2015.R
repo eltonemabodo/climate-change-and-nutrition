f99df6ab5ef600115c7f8e320b9dacf60bb13dd7
iammabodo <eltone.mabodo@gmail.com>
2025-01-14 15:08:50 +0200

    Added the miscellaneous file and started reading the 2015 ZDHS
    The miscellaneous folder contains other information which may not be necessarily used for the "actual" analysis. As of now, this folder contains only the latest DHS codebook for the definition of variables

--- code/zdhs_2015.R
+++ code/zdhs_2015.R
@@ -1,3 +1,9 @@
 ## Loading all the necessary libraries
 
-library(tidyverse)
+library(tidyverse) # main library for data manipulation
+library(haven) # for reading SPSS and Stata files
+library(readxl) # for reading excel files
+
+
+# Reading the data
+zdhs_2015 <- read_dta("climate-change-and-nutrition/data /raw data/zdhs_2015.DTA")
