4b24de2d51251a7965fe6664d3b405d981dfd8f1
iammabodo <eltone.mabodo@gmail.com>
2025-02-09 14:57:37 +0700

    Climate Visualisation
    Drawn a bivariate map linking temperature and rainfall patterns between 2000 and 2015

--- code/geodata.R
+++ code/geodata.R
@@ -4,6 +4,14 @@ library(tidyverse)
 library(sf)
 library(geodata)
 library(lubridate)
+library(biscale)
+library(showtext)
+
+
+# Custom Fonts for the plot
+font_add_google("EB Garamond", "garamond")
+showtext_auto()
+
 
 
 # Getting the climate data for Zimbabwe
@@ -17,35 +25,102 @@ precipitation_data <- read_xlsx("data /raw data/climate data/precipitation.xlsx"
   pivot_longer(cols = -name, names_to = "date", values_to = "precipitation") %>%
   # Convert the date to a date object
   mutate(date = ym(date)) %>%
-  filter(date >= ym("2000-01")) %>% 
+  filter(date >= ym("1995-01")) %>% 
   mutate(month = month(date),
          year = year(date)) %>% 
   # filter only the farming season
   filter(month == 1 | month == 10) %>% 
   mutate(DHSYear = case_when(
-    year == 2000 | year == 2001 | year == 2002 | year == 2003 | year == 2004 | year == 2005 ~ 2005,
+    year  == 2001 | year == 2002 | year == 2003 | year == 2004 | year == 2005 ~ 2005,
     year == 2006 | year == 2007 | year == 2008 | year == 2009 | year == 2010   ~ 2010,
     year == 2011 | year == 2012 | year == 2013 | year == 2014 | year == 2015   ~ 2015,
-    TRUE ~ NA
+    TRUE ~ 2000
   )) %>% 
-  filter(!is.na(DHSYear))
+  filter(year <= 2015)
 
 # Rainfall during the rain season by province and dhs year
 rainfall_province <- precipitation_data %>% 
   group_by(name, DHSYear) %>% 
   summarise(rainfall = mean(precipitation, na.rm = TRUE)) 
 
-# Merge the rainfall data with the province boundaries
-province_boundaries <- left_join(province_boundaries, rainfall_province, by = c("ADM1_EN" = "name"))
-  
-# Plot the rainfall data
-ggplot(province_boundaries) +
-  geom_sf(aes(fill = rainfall)) +
-  facet_wrap(~DHSYear) +
-  scale_fill_viridis_c() +
-  theme_minimal() +
-  labs(title = "Average Rainfall by Province",
-       fill = "Rainfall (mm)",
-       caption = "Source: Zimbabwe Meteorological Services") +
-  theme(legend.position = "bottom") +
-  theme(plot.title = element_text(hjust = 0.5))
+
+# Max Temperature data
+max_temperature_data <- read_xlsx("data /raw data/climate data/maxtemperature.xlsx") %>% 
+  filter(name != "Zimbabwe") %>%
+  # pivot the data
+  pivot_longer(cols = -name, names_to = "date", values_to = "max_temperature") %>%
+  # Convert the date to a date object
+  mutate(date = ym(date)) %>%
+  filter(date >= ym("1995-01")) %>% 
+  mutate(month = month(date),
+         year = year(date)) %>% 
+  mutate(DHSYear = case_when(
+    year  == 2001 | year == 2002 | year == 2003 | year == 2004 | year == 2005 ~ 2005,
+    year == 2006 | year == 2007 | year == 2008 | year == 2009 | year == 2010   ~ 2010,
+    year == 2011 | year == 2012 | year == 2013 | year == 2014 | year == 2015   ~ 2015,
+    TRUE ~ 2000
+  )) %>% 
+  filter(year <= 2015)
+
+# Max Temperature during the rain season by province and dhs year
+
+max_temperature_province <- max_temperature_data %>% 
+  group_by(name, DHSYear) %>% 
+  summarise(max_temperature = mean(max_temperature, na.rm = TRUE))
+
+## Climate Province Data 
+climate_province_data <- rainfall_province %>% 
+  left_join(max_temperature_province, by = c("name", "DHSYear")) 
+
+# Join. the climate data with provincial polygons
+climate_province_data <- province_boundaries %>% 
+  left_join(climate_province_data, by = c("ADM1_EN" = "name")) %>% 
+  st_transform(crs = 4326)
+
+# Create biclass for the climate data
+
+climate_province_data <- bi_class(climate_province_data, 
+                                  x = "rainfall", 
+                                  y = "max_temperature", 
+                                  dim = 3, 
+                                  style = "jenks")
+
+
+pallet <- "BlueOr"
+# Plot the bivariate map
+ggplot(data = climate_province_data) +
+  geom_sf(aes(fill = bi_class), color = "black") + # Map `fill` to the bivariate class
+  bi_scale_fill(pal = pallet, dim = 3) +
+  facet_wrap(~DHSYear, nrow = 1) +
+  theme_void() +
+  labs(title = "Climate Variation in Zimbabwe by Province",
+       subtitle = "Average rainifall during the peak of the farming season and mean annual maximum temperature over time",
+       caption = "Source: Author's calculations using World Bank Climate Data") +
+  theme(
+    text = element_text(family = "garamond"),
+    plot.title = element_text(size = 12, face = "bold",
+                              margin = margin(b = 3, l = 10)),
+    plot.subtitle = element_text(size = 10, margin = margin(b = 10, l = 10)),
+    plot.caption = element_text(size = 7, margin = margin(t = 10, l = 10),
+                                hjust = 0),
+    legend.position = "none"
+  )
+
+# Legend of bivariate map
+
+legend <- bi_legend(pal = pallet,   
+                    flip_axes = FALSE,
+                    rotate_pal = FALSE,
+                    dim = 3,
+                    xlab = "Mean Annual Max Temperature (degrees)",
+                    ylab = "Mean Farming Season Precipitation (mm)",
+                    size = 10)
+
+
+
+
+
+
+
+
+
